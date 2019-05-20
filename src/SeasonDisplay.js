649aaffdef6b9e3b891aa42bf3b9f77aa1e912e0
serpro <serproqnx@users.noreply.github.com>
2019-05-20 18:29:06 +0300

    48. Getting a user physical location. works

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-const SeasonDispay = () => {
+const SeasonDisplay = () => {
+  
   return <div>Season Display</div>
 }
 
-export default SeasonDispay
+export default SeasonDisplay
