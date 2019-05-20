3c80fc748190882a32a09e309c2fe50b60abc99f
serpro <serproqnx@users.noreply.github.com>
2019-05-20 16:25:41 +0300

    it works

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          start coding
         </a>
       </header>
     </div>
