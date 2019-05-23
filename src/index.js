c118419009ab04d89651a477ef7d4c6910da719d
serpro <serproqnx@users.noreply.github.com>
2019-05-23 18:29:20 +0300

    54.Updatig STate Properties

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
+  
+  constructor() {
+    super(props)
+    
+    this.state = { lat: }
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       position => console.log(position),
