3bc72766014a272d62a489e9624ca2c29b25e317
serpro <serpro.qnx@gmail.com>
2019-09-15 15:14:10 +0300

    60.Faking Requiests with Moxios

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { mount } from 'enzyme'
+import moxios from 'moxios'
 import Root from 'Root'
 import App from 'components/App'
+
+beforeEach(() => {
+  moxios.install()
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
+  })
+})
+
+afterEach(() => {
+  moxios.uninstall()
+})
 
 it('can fetch a list of comments and display them', () => {
   const wrapped = mount(
@@ -12,5 +25,5 @@ it('can fetch a list of comments and display them', () => {
 
   wrapped.find('.fetch-comments').simulate('click')
 
-  expect(wrapped.find('li').length).toEqual(500)
+  expect(wrapped.find('li').length).toEqual(2)
 })
