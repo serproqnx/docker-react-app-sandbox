2485daa584774ea83d445eb7800f7d81bf9cb21a
serpro <serpro.qnx@gmail.com>
2019-09-15 12:48:36 +0300

    46.Handling Unknown Types

--- src/reducers/__test__/comments.test.js
+++ src/reducers/__test__/comments.test.js
@@ -11,3 +11,9 @@ it('handles actions of type SAVE_COMMENT', () => {
 
   expect(newState).toEqual(['New Comment'])
 })
+
+it('handles action with unknown type', () => {
+  const newState = commentsReducer([], { type: 'sdfvsdfv'})
+
+  expect(newState).toEqual([])
+})
