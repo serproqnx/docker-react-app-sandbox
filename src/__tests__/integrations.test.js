8f177c98c0a3003892205e16ac133bf4b5254a8e
serpro <serpro.qnx@gmail.com>
2019-09-15 15:26:47 +0300

    62.Introducing a Pause

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -6,7 +6,7 @@ import App from 'components/App'
 
 beforeEach(() => {
   moxios.install()
-  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+  moxios.stubRequest('https://jsonplaceholder.typicode.com/comments', {
     status: 200,
     response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
   })
@@ -16,7 +16,7 @@ afterEach(() => {
   moxios.uninstall()
 })
 
-it('can fetch a list of comments and display them', () => {
+it('can fetch a list of comments and display them', (done) => {
   const wrapped = mount(
     <Root>
       <App />
@@ -25,5 +25,11 @@ it('can fetch a list of comments and display them', () => {
 
   wrapped.find('.fetch-comments').simulate('click')
 
-  expect(wrapped.find('li').length).toEqual(2)
+  setTimeout(() => {
+    wrapped.update()
+
+    expect(wrapped.find('li').length).toEqual(3)
+
+    done()
+  }, 100)
 })
