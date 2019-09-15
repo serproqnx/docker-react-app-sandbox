b5f9394e458f6dabcf53769265facbc966706610
serpro <serpro.qnx@gmail.com>
2019-09-15 13:47:37 +0300

    50.Redux initial State

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { mount } from 'enzyme'
-
+import Root from '../../Root'
 import CommentList from 'components/CommentList'
 
+let wrapped
+
 beforeEach(() => {
+  const initialState = {
+    comments: ['Comment 1', 'Comment 2']
+  }
+
   wrapped = mount(
-    <Root>
+    <Root initialState={initialState}>
       <CommentList />
     </Root>
   )
 })
 
 it('create one LI per comment', () => {
-
+  console.log(wrapped.find('li').length);
 })
