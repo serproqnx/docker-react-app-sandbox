32b1807b4e18d87f4887a702a0093e9a38d6f3e9
serpro <serpro.qnx@gmail.com>
2019-09-17 07:24:45 +0300

    testing finish

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -25,11 +25,11 @@ it('can fetch a list of comments and display them', (done) => {
 
   wrapped.find('.fetch-comments').simulate('click')
 
-  setTimeout(() => {
+  moxios.wait(() => {
     wrapped.update()
 
-    expect(wrapped.find('li').length).toEqual(3)
+    expect(wrapped.find('li').length).toEqual(2)
 
     done()
-  }, 100)
+  })
 })
