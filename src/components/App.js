7f1a9bda53591c348affc16a853c6f00252cd76d
serpro <serproqnx@users.noreply.github.com>
2019-09-23 11:26:13 +0300

    stream pulse works with an example

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
+import GraphTest from 'components/BarChartV1'
+import Test from 'components/Test'
+// import ChartWrapper from 'components/ChartWrapper.js'
+import ChartWrapper from 'components/D3StreamGraphWrapper'
 
-const App = () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ChartWrapper />
+        {/* <CommentBox /> */}
+        {/* <CommentList /> */}
+      </div>
+    )
+  }
 }
 
 export default App
